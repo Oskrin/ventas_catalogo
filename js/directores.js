638cda5b7fc53f2d55fb3a399380f6b3a818b056
Oskrin <oskrs11@hotmail.com>
2014-11-25 12:41:02 -0500

    validacion
    repetidos

--- js/directores.js
+++ js/directores.js
@@ -8,6 +8,7 @@ function scrollToBottom() {
         scrollTop: $(document).height()
     }, 'slow');
 }
+
 function scrollToTop() {
     $('html, body').animate({
         scrollTop: 0
@@ -107,47 +108,47 @@ function modificar_directores() {
     if ($("#id_director").val() === "") {
         alertify.alert("Seleccione un director@");
     } else {
-            if (iden.length < 10) {
-                $("#ruc_ci").focus();
-                alertify.alert("Error.. Mínimo 10 digitos ");
+        if (iden.length < 10) {
+            $("#ruc_ci").focus();
+            alertify.alert("Error.. Mínimo 10 digitos ");
+        } else {
+            if ($("#nombres_cli").val() === "") {
+                $("#nombres_cli").focus();
+                alertify.alert("Ingrese Nombres completos");
             } else {
-                    if ($("#nombres_cli").val() === "") {
-                        $("#nombres_cli").focus();
-                        alertify.alert("Ingrese Nombres completos");
+                if ($("#pais_cli").val() === "") {
+                    $("#pais_cli").focus();
+                    alertify.alert("Ingrese un pais");
+                } else {
+                    if ($("#ciudad_cli").val() === "") {
+                        $("#ciudad_cli").focus();
+                        alertify.alert("Ingrese una ciudad");
                     } else {
-                               if ($("#pais_cli").val() === "") {
-                                    $("#pais_cli").focus();
-                                    alertify.alert("Ingrese un pais");
-                                } else {
-                                    if ($("#ciudad_cli").val() === "") {
-                                        $("#ciudad_cli").focus();
-                                        alertify.alert("Ingrese una ciudad");
-                                    } else {
-                                        if ($("#direccion_cli").val() === "") {
-                                            $("#direccion_cli").focus();
-                                            alertify.alert("Ingrese una dirección");
-                                        }else{
-                                            $.ajax({
-                                                type: "POST",
-                                                url: "../procesos/modificar_directores.php",
-                                                data: "ruc_ci=" + $("#ruc_ci").val() + "&id_director=" + $("#id_director").val() +
-                                                "&nombres_cli=" + $("#nombres_cli").val() + "&direccion_cli=" + $("#direccion_cli").val() + "&nro_telefono=" + $("#nro_telefono").val() + "&nro_celular=" + $("#nro_celular").val() + "&pais_cli=" + $("#pais_cli").val() + "&ciudad_cli=" + $("#ciudad_cli").val() + "&email=" + $("#email").val(),
-                                                success: function(data) {
-                                                    var val = data;
-                                                    if (val == 1) {
-                                                        alertify.alert("Datos Modificados Correctamente",function(){
-                                                            location.reload();
-                                                        });
-                                                    }
-                                                }
-                                            });  
-                                        }
+                        if ($("#direccion_cli").val() === "") {
+                            $("#direccion_cli").focus();
+                            alertify.alert("Ingrese una dirección");
+                        }else{
+                            $.ajax({
+                                type: "POST",
+                                url: "../procesos/modificar_directores.php",
+                                data: "ruc_ci=" + $("#ruc_ci").val() + "&id_director=" + $("#id_director").val() +
+                                "&nombres_cli=" + $("#nombres_cli").val() + "&direccion_cli=" + $("#direccion_cli").val() + "&nro_telefono=" + $("#nro_telefono").val() + "&nro_celular=" + $("#nro_celular").val() + "&pais_cli=" + $("#pais_cli").val() + "&ciudad_cli=" + $("#ciudad_cli").val() + "&email=" + $("#email").val(),
+                                success: function(data) {
+                                    var val = data;
+                                    if (val == 1) {
+                                        alertify.alert("Datos Modificados Correctamente",function(){
+                                            location.reload();
+                                        });
                                     }
                                 }
-                            }
+                            });  
                         }
                     }
                 }
+            }
+        }
+    }
+}
 
 function eliminar_directores() {
     if ($("#id_director").val() === "") {
@@ -246,103 +247,12 @@ function inicio() {
         strict: false
     });
     ///////////////////////////////// 
-
-    ///////////////validacion documentos////////////
-    $("#ruc_ci").keyup(function() {   
-        var numero = $("#ruc_ci").val();
-        var suma = 0;      
-        var residuo = 0;      
-        var pri = false;      
-        var pub = false;            
-        var nat = false;                     
-        var modulo = 11;
-        var p1;
-        var p2;
-        var p3;
-        var p4;
-        var p5;
-        var p6;
-        var p7;
-        var p8;            
-        var p9; 
-        var d1  = numero.substr(0,1);         
-        var d2  = numero.substr(1,1);         
-        var d3  = numero.substr(2,1);         
-        var d4  = numero.substr(3,1);         
-        var d5  = numero.substr(4,1);         
-        var d6  = numero.substr(5,1);         
-        var d7  = numero.substr(6,1);         
-        var d8  = numero.substr(7,1);         
-        var d9  = numero.substr(8,1);         
-        var d10 = numero.substr(9,1);  
-
-        if (d3 < 6){           
-            nat = true;            
-            p1 = d1 * 2;
-            if (p1 >= 10) p1 -= 9;
-            p2 = d2 * 1;
-            if (p2 >= 10) p2 -= 9;
-            p3 = d3 * 2;
-            if (p3 >= 10) p3 -= 9;
-            p4 = d4 * 1;
-            if (p4 >= 10) p4 -= 9;
-            p5 = d5 * 2;
-            if (p5 >= 10) p5 -= 9;
-            p6 = d6 * 1;
-            if (p6 >= 10) p6 -= 9; 
-            p7 = d7 * 2;
-            if (p7 >= 10) p7 -= 9;
-            p8 = d8 * 1;
-            if (p8 >= 10) p8 -= 9;
-            p9 = d9 * 2;
-            if (p9 >= 10) p9 -= 9;             
-            modulo = 10;
-        } else if(d3 == 6){           
-            pub = true;             
-            p1 = d1 * 3;
-            p2 = d2 * 2;
-            p3 = d3 * 7;
-            p4 = d4 * 6;
-            p5 = d5 * 5;
-            p6 = d6 * 4;
-            p7 = d7 * 3;
-            p8 = d8 * 2;            
-            p9 = 0;            
-        } else if(d3 == 9) {          
-            pri = true;                                   
-            p1 = d1 * 4;
-            p2 = d2 * 3;
-            p3 = d3 * 2;
-            p4 = d4 * 7;
-            p5 = d5 * 6;
-            p6 = d6 * 5;
-            p7 = d7 * 4;
-            p8 = d8 * 3;
-            p9 = d9 * 2;            
-        }
-                
-        suma = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;                
-        residuo = suma % modulo;                                         
-
-        var digitoVerificador = residuo==0 ? 0: modulo - residuo; 
-        ////////////verificamos validacioncedula////////////////////
-        if (numero.length === 10) {
-            if(nat == true){
-                if (digitoVerificador != d10){                          
-                    alertify.error('El número de cédula es incorrecto.');
-                }else{
-                    alertify.success('El número de cédula es correcto.');
-                }
-            }
-        }
-    });
- 
-
+   
     /////valida si ya existe/////
     $("#ruc_ci").keyup(function() {
         $.ajax({
             type: "POST",
-            url: "../procesos/comparar_cedulas.php",
+            url: "../procesos/comparar_cedulas3.php",
             data: "cedula=" + $("#ruc_ci").val(),
             success: function(data) {
                 var val = data;
@@ -350,6 +260,93 @@ function inicio() {
                     $("#ruc_ci").val("");
                     $("#ruc_ci").focus();
                     alertify.error("Error... Director@ registrado");
+                }else{
+                    var numero = $("#ruc_ci").val();
+                    var suma = 0;      
+                    var residuo = 0;      
+                    var pri = false;      
+                    var pub = false;            
+                    var nat = false;                     
+                    var modulo = 11;
+                    var p1;
+                    var p2;
+                    var p3;
+                    var p4;
+                    var p5;
+                    var p6;
+                    var p7;
+                    var p8;            
+                    var p9; 
+                    var d1  = numero.substr(0,1);         
+                    var d2  = numero.substr(1,1);         
+                    var d3  = numero.substr(2,1);         
+                    var d4  = numero.substr(3,1);         
+                    var d5  = numero.substr(4,1);         
+                    var d6  = numero.substr(5,1);         
+                    var d7  = numero.substr(6,1);         
+                    var d8  = numero.substr(7,1);         
+                    var d9  = numero.substr(8,1);         
+                    var d10 = numero.substr(9,1);  
+
+                    if (d3 < 6){           
+                        nat = true;            
+                        p1 = d1 * 2;
+                        if (p1 >= 10) p1 -= 9;
+                        p2 = d2 * 1;
+                        if (p2 >= 10) p2 -= 9;
+                        p3 = d3 * 2;
+                        if (p3 >= 10) p3 -= 9;
+                        p4 = d4 * 1;
+                        if (p4 >= 10) p4 -= 9;
+                        p5 = d5 * 2;
+                        if (p5 >= 10) p5 -= 9;
+                        p6 = d6 * 1;
+                        if (p6 >= 10) p6 -= 9; 
+                        p7 = d7 * 2;
+                        if (p7 >= 10) p7 -= 9;
+                        p8 = d8 * 1;
+                        if (p8 >= 10) p8 -= 9;
+                        p9 = d9 * 2;
+                        if (p9 >= 10) p9 -= 9;             
+                        modulo = 10;
+                    } else if(d3 == 6){           
+                        pub = true;             
+                        p1 = d1 * 3;
+                        p2 = d2 * 2;
+                        p3 = d3 * 7;
+                        p4 = d4 * 6;
+                        p5 = d5 * 5;
+                        p6 = d6 * 4;
+                        p7 = d7 * 3;
+                        p8 = d8 * 2;            
+                        p9 = 0;            
+                    } else if(d3 == 9) {          
+                        pri = true;                                   
+                        p1 = d1 * 4;
+                        p2 = d2 * 3;
+                        p3 = d3 * 2;
+                        p4 = d4 * 7;
+                        p5 = d5 * 6;
+                        p6 = d6 * 5;
+                        p7 = d7 * 4;
+                        p8 = d8 * 3;
+                        p9 = d9 * 2;            
+                    }
+                
+                    suma = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;                
+                    residuo = suma % modulo;                                         
+
+                    var digitoVerificador = residuo==0 ? 0: modulo - residuo; 
+                    ////////////verificamos validacioncedula////////////////////
+                    if (numero.length === 10) {
+                        if(nat == true){
+                            if (digitoVerificador != d10){                          
+                                alertify.error('El número de cédula es incorrecto.');
+                            }else{
+                                alertify.success('El número de cédula es correcto.');
+                            }
+                        }
+                    }
                 }
             }
         });
@@ -390,11 +387,9 @@ function inicio() {
     $("#directores").dialog(dialogo);
     $("#clave_permiso").dialog(dialogo3);
     $("#seguro").dialog(dialogo4);
-/////////////////////////// 
+    /////////////////////////// 
 
- 
-///////////////////////////////////////
-  /////////////tabla clientes/////////
+    /////////////tabla clientes/////////
     jQuery("#list").jqGrid({
         url: '../xml/datos_directores.php',
         datatype: 'xml',
