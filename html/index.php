8bf1cf134cd389a8b9c8a7da5a63ca2257c51352
deividscriollo <deividscriollo@gmail.com>
2014-12-01 15:25:25 -0500

    boton

--- html/index.php
+++ html/index.php
@@ -14,7 +14,7 @@ session_destroy();
         <link href="../css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" type="text/css" href="../css/jquery-ui-1.10.4.custom.css"/> 
         <link href="../css/font-awesome.css" rel="stylesheet">
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600" rel="stylesheet">
+        
         <link href="../css/style.css" rel="stylesheet" type="text/css">
         <link href="../css/pages/signin.css" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="../css/alertify.core.css" />
