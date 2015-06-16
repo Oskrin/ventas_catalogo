98b9571686b537226e7c9a68db01df32def516b6
thyrons <willynarvaez6@gmail.com>
2015-06-16 16:06:18 -0500

    CAMBIOS
    CAMBIOS

--- reportes/reportes/reporte_director_agrupado_facturas.php
+++ reportes/reportes/reporte_director_agrupado_facturas.php
@@ -181,55 +181,7 @@
             }           
 
             
-        }
-        // $sql1 = pg_query("SELECT productos.cod_productos,productos.codigo,articulo,categoria,marca,sum(cantidad::int),precio_venta,detalle_factura_venta.total_venta from factura_venta,clientes,directores,detalle_factura_venta,productos where clientes.id_director = directores.id_director and factura_venta.id_cliente = clientes.id_cliente and detalle_factura_venta.id_factura_venta = factura_venta.id_factura_venta and detalle_factura_venta.cod_productos = productos.cod_productos and directores.id_director = '".$_GET['id']."' and clientes.id_cliente = '".$row[0]."' and  fecha_actual between '".$_GET['inicio']."' and '".$_GET['fin']."' group by  productos.cod_productos,precio_venta,detalle_factura_venta.total_venta, productos.codigo,productos.articulo,productos.categoria,productos.marca");
-        // if(pg_num_rows($sql1)){
-        //     $pdf->SetX(1);                
-        //     $pdf->Cell(25, 6, utf8_decode("EMPRESARI@"),1,0, 'C',1);
-        //     $pdf->Cell(60, 6, maxCaracter(utf8_decode($row[2]),50),1,0, 'L',1);
-        //     $pdf->Cell(20, 6, utf8_decode("CI/RUC"),1,0, 'C',1);        
-        //     $pdf->Cell(40, 6, maxCaracter(utf8_decode($row[1]),15),1,0, 'L',1);                            
-        //     $pdf->Cell(40, 6, utf8_decode("COSTO PVP"),1,0, 'C',1);        
-        //     $pdf->Cell(20, 6, utf8_decode("DESCUENTO"),1,1, 'L',1); 
-        //     $pdf->SetX(1);  
-        //     $pdf->Cell(25, 6, utf8_decode("COD"),1,0, 'C',0);       
-        //     $pdf->Cell(60, 6, utf8_decode("DESCRIPCIÓN"),1,0, 'C',0);       
-        //     $pdf->Cell(20, 6, utf8_decode("CATEGORÍA"),1,0, 'C',0);       
-        //     $pdf->Cell(20, 6, utf8_decode("MARCA"),1,0, 'C',0);       
-        //     $pdf->Cell(20, 6, utf8_decode("CANT"),1,0, 'C',0);       
-        //     $pdf->Cell(20, 6, utf8_decode("VALOR UNI"),1,0, 'C',0);       
-        //     $pdf->Cell(20, 6, utf8_decode("VALOR TOT"),1,0, 'C',0);       
-        //     $pdf->Cell(20, 6, utf8_decode("30%"),1,1, 'C',0);      
-
-        //     while ($row1 = pg_fetch_row($sql1)) {            
-        //         $temp = $row1[0];
-        //         $desc = 0;
-        //         $pdf->SetX(1);  
-        //         $pdf->Cell(25, 6, maxCaracter(utf8_decode($row1[1]),15),0,0, 'L',0);
-        //         $pdf->Cell(60, 6, maxCaracter(utf8_decode($row1[2]),30),0,0, 'L',0);
-        //         $pdf->Cell(20, 6, maxCaracter(utf8_decode($row1[3]),10),0,0, 'L',0);
-        //         $pdf->Cell(20, 6, maxCaracter(utf8_decode($row1[4]),10),0,0, 'L',0);
-        //         $pdf->Cell(20, 6, maxCaracter(utf8_decode($row1[5]),50),0,0, 'C',0);
-        //         $pdf->Cell(20, 6, maxCaracter(utf8_decode($row1[6]),50),0,0, 'C',0);
-        //         $pdf->Cell(20, 6, maxCaracter(utf8_decode($row1[6] * $row1[5]),50),0,0, 'C',0);
-        //         $desc = ($row1[6] * $row1[5]) * 0.30;
-        //         $desc = ($row1[6] * $row1[5]) - $desc;
-        //         $pdf->Cell(20, 6, maxCaracter(number_format($desc,3,',','.'),10),0,1, 'C',0);
-        //         $cant = $cant + $row1[5];
-        //         $valor_uni = $valor_uni + $row1[6];
-        //         $valor_tot = $valor_tot + ($row1[6] * $row1[5]);
-        //         $desc_tot = $desc_tot + $desc;
-        //         $total_final = $total_final + $desc;
-        //     }         
-        //     $pdf->SetX(1);                                             
-        //     $pdf->Cell(205, 0, utf8_decode(""),1,1, 'R',0);
-        //     $pdf->Cell(126, 6, utf8_decode("Totales"),0,0, 'R',0);
-        //     $pdf->Cell(20, 6, maxCaracter((number_format($cant,2,',','.')),20),0,0, 'C',0);
-        //     $pdf->Cell(20, 6, maxCaracter((number_format($valor_uni,2,',','.')),20),0,0, 'C',0);
-        //     $pdf->Cell(20, 6, maxCaracter((number_format($valor_tot,2,',','.')),20),0,0, 'C',0);
-        //     $pdf->Cell(20, 6, maxCaracter((number_format($desc_tot,2,',','.')),20),0,1, 'C',0);
-        //     $pdf->Ln(3);
-        // }        
+        }        
     }
     $pdf->SetX(1);                                             
     $pdf->Ln(8);              
